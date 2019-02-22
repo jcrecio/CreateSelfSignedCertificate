e1b6fbcb47e16473f77a0d54d1265d6f0634d2bb
Recio Abad <JuanReci@youforce.net>
2019-02-22 15:02:10 +0100

    Added script for certificate generation

---
+++ CreateSelfSignedCertificate.ps1
@@ -0,0 +1,9 @@
+﻿function Create-SelfSignedCertificateInFile{
+  [CmdletBinding()]
+  param(
+    [string] $filePath,
+    [string] $password)
+        $certData = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname localhost
+        $pwd = ConvertTo-SecureString -String $password -Force -AsPlainText
+        Export-PfxCertificate -cert "cert:\localMachine\my\$($certData.Thumbprint)" -FilePath $filePath -Password $pwd
+}
